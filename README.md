78323d3671f096d2ad1f188d84cfdbd82025d663
mantou <mantou@meiyaapp.com>
2015-08-24 16:26:14 +0800

    添加ForegroundImageView

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ForegroundViews
+
+The origin source code is writed by JakeWharton, https://gist.github.com/JakeWharton/0a251d67649305d84e8a
+
+Only support ImageViews for now.
---
+++ sample/src/main/java/me/fanxu123/foregrounds/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package me.fanxu123.foregrounds.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * For test
+ * Created by fanxu123 on 15/8/24.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fv_main);
+    }
+}
